32721259169d25c869a3e442247dd82508a438b1
sehun-kim <rogarithm@gmail.com>
2023-01-02 13:44:00 +0900

    initialize git repo

---
+++ cdb.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+function cdb() {
+	USAGE="Usage: cdb [-c|-g|-d|-l] [bookmark]" ;
+	if  [ ! -e ~/.cd_bookmarks ] ; then
+		mkdir ~/.cd_bookmarks
+	fi
+
+	case $1 in
+		# create bookmark
+		-c) shift
+		if [ ! -f ~/.cd_bookmarks/$1 ] ; then
+			echo "cd `pwd`" > ~/.cd_bookmarks/"$1" ;
+		else
+			echo "Try again! Looks like there is already a bookmark '$1'"
+		fi
+		;;
+		# goto bookmark
+		-g) shift
+		if [ -f ~/.cd_bookmarks/$1 ] ; then 
+			source ~/.cd_bookmarks/"$1"
+		else
+			echo "Looks like your bookmark does not exist." ;
+		fi
+		;;
+		# delete bookmark
+		-d) shift
+		if [ -f ~/.cd_bookmarks/$1 ] ; then 
+			rm ~/.cd_bookmarks/"$1" ;
+		else
+			echo "Oops, forgot to specify the bookmark" ;
+		fi    
+		;;
+		# list bookmarks
+		-l) shift
+		cd ~/.cd_bookmarks/ ;
+		for file in $(ls)
+		do
+			echo "${file}: $(cat ${file})"
+		done
+		;;
+	  *) echo "$USAGE" ;
+		;;
+esac
+}
