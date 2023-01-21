19eb1c05770d512f5ae36e253293fe7834f05e13
sehun-kim <rogarithm@gmail.com>
2023-01-21 22:43:57 +0900

    정의한 함수를 호출해서 기능을 실행한다

--- ledger/range.sh
+++ ledger/range.sh
@@ -14,11 +14,14 @@ range () {
 	gsed -e '1d' ${SOURCE_DIR}/${SOURCE_FILE} | gsed -e '/^$/d' | gsed 's/ | /\t/g' | grep -e ${DAY} | cut -f3 > ${TMP_DIR}/${TMP_FILE}
 }
 
-clear
 printRange () {
 	gsed -e '1d' ${SOURCE_DIR}/${SOURCE_FILE} | gsed -e '/^$/d' | gsed 's/ | /\t/g' | grep -e ${DAY} | cat
 }
+
+clear
+printRange ${DAY}
 echo
+range ${DAY}
 (cat ${TMP_DIR}/${TMP_FILE} | tr '\012' '+'; echo "0") | bc
 
 #cat ${TMP_DIR}/${TMP_FILE}
