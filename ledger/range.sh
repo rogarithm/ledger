e32126344c98006ea507b3cff7e56277f4baf6a1
sehun-kim <rogarithm@gmail.com>
2023-01-21 22:37:25 +0900

    계산 중간 결과 확인용 출력문 추가

--- ledger/range.sh
+++ ledger/range.sh
@@ -12,6 +12,8 @@ fi
 gsed -e '1d' ${SOURCE_DIR}/${SOURCE_FILE} | gsed -e '/^$/d' | gsed 's/ | /\t/g' | grep -e '01.01' | cut -f3 > ${TMP_DIR}/${TMP_FILE}
 
 clear
+gsed -e '1d' ${SOURCE_DIR}/${SOURCE_FILE} | gsed -e '/^$/d' | gsed 's/ | /\t/g' | grep -e '01.01' | cat
+echo
 (cat ${TMP_DIR}/${TMP_FILE} | tr '\012' '+'; echo "0") | bc
 
 #cat ${TMP_DIR}/${TMP_FILE}
