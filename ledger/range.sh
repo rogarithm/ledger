6ce5aec96af15ef5ab58dd43e91c29a046912e06
sehun-kim <rogarithm@gmail.com>
2023-01-21 22:41:35 +0900

    인자에 이름을 붙인다

--- ledger/range.sh
+++ ledger/range.sh
@@ -4,15 +4,16 @@ SOURCE_DIR='/Users/sehun/personal/ledger'
 SOURCE_FILE='2023-01.txt'
 TMP_DIR='tmp'
 TMP_FILE='range'
+DAY=$1
 
 if [ ! -d ${TMP_DIR}/${TMP_FILE} ]; then
 	touch ${TMP_DIR}/${TMP_FILE}
 fi
 
-gsed -e '1d' ${SOURCE_DIR}/${SOURCE_FILE} | gsed -e '/^$/d' | gsed 's/ | /\t/g' | grep -e ${1} | cut -f3 > ${TMP_DIR}/${TMP_FILE}
+gsed -e '1d' ${SOURCE_DIR}/${SOURCE_FILE} | gsed -e '/^$/d' | gsed 's/ | /\t/g' | grep -e ${DAY} | cut -f3 > ${TMP_DIR}/${TMP_FILE}
 
 clear
-gsed -e '1d' ${SOURCE_DIR}/${SOURCE_FILE} | gsed -e '/^$/d' | gsed 's/ | /\t/g' | grep -e ${1} | cat
+gsed -e '1d' ${SOURCE_DIR}/${SOURCE_FILE} | gsed -e '/^$/d' | gsed 's/ | /\t/g' | grep -e ${DAY} | cat
 echo
 (cat ${TMP_DIR}/${TMP_FILE} | tr '\012' '+'; echo "0") | bc
 
