2513996581c5dfb8cd62931e4eecadfd855a5b7a
sehun-kim <rogarithm@gmail.com>
2023-01-29 00:22:30 +0900

    목적에 더 알맞은 함수 이름으로 변경

--- ledger/range.sh
+++ ledger/range.sh
@@ -31,7 +31,7 @@ _extract_dates_in_range () {
 	done < ${TMP_DIR}/${DATES_FILE}
 }
 
-range () {
+_extract_payments_of_day () {
 	gsed -e '1d' ${SOURCE_DIR}/${SOURCE_FILE} | gsed -e '/^$/d' | gsed 's/ | /\t/g' | grep -e $1
 }
 
@@ -44,9 +44,9 @@ clear
 echo "특정일의 총 지출액 계산 테스트"
 DAY='01.01'
 echo "특정일의 지출 내역 전체를 출력"
-range ${DAY} | cat
+_extract_payments_of_day ${DAY} | cat
 echo "특정일의 지출 금액만 임시 파일에 저장"
-range ${DAY} | cut -f3 > ${TMP_DIR}/${TMP_FILE}
+_extract_payments_of_day ${DAY} | cut -f3 > ${TMP_DIR}/${TMP_FILE}
 echo "특정일의 지출 금액 총합 계산"
 (cat ${TMP_DIR}/${TMP_FILE} | tr '\012' '+'; echo "0") | bc
 _print_empty_line
@@ -58,6 +58,6 @@ echo "기간 내 지출 내역 추출 테스트"
 echo "임시 파일에 저장된 날짜 모두에 대해 지출 내역 전체를 출력"
 while read DATE; do
 	if [ ${DATE} ]; then
-		range ${DATE} | cat
+		_extract_payments_of_day ${DATE} | cat
 	fi
 done < ${TMP_DIR}/${DATES_FILE}
