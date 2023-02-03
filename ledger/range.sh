f635ce8a83adff475c4be104f8851b82683b8324
sehun-kim <rogarithm@gmail.com>
2023-02-03 16:33:11 +0900

    함수 내에서 쓰는 매개변수가 담당하는 역할을 이름으로 붙인다

--- ledger/range.sh
+++ ledger/range.sh
@@ -23,17 +23,20 @@ _extract_dates_in_range () {
 _extract_payments_of_day () {
 	local source_dir='/Users/sehun/personal/ledger'
 	local source_file='2023-01.txt'
+
 	gsed -e '1d' ${source_dir}/${source_file} | gsed -e '/^$/d' | gsed 's/ | /\t/g' | grep -e $1
 }
 
 compute_total_amounts_in_range () {
 	local tmp_file='payments_in_range'
+	local start_date=$1
+	local end_date=$2
 
 	if [ ! -d ${TMP_DIR}/${tmp_file} ]; then
 		touch ${TMP_DIR}/${tmp_file}
 	fi
 
-	_extract_dates_in_range $1 $2
+	_extract_dates_in_range ${start_date} ${end_date}
 
 	cat /dev/null > ${TMP_DIR}/${tmp_file}
 
