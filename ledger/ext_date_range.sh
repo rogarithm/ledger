2eddeb4ecf152cfba56730d184584e4081311c3d
sehun-kim <rogarithm@gmail.com>
2023-03-07 19:11:00 +0900

    임시 파일 필요 없는 _extract_dates_in_range 구현

---
+++ ledger/ext_date_range.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+start_at=$1
+end_at=$2
+
+extract_dates_in_range () {
+
+	result=${start_at}
+	while [ "${start_at}" != "${end_at}" ]; do
+		start_at=$(date -j -f '%m.%d' -v+1d ${start_at} +%m.%d)
+		result=$result:${start_at}
+	done
+
+	echo $result
+	exit 0
+}
+
+extract_dates_in_range $start_at $end_at
