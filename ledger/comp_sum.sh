961f017889ebbe80327a7a3f7a88f74116688d0f
sehun-kim <rogarithm@gmail.com>
2023-03-14 16:41:03 +0900

    지출 내역 반환 데이터 형식 변경을 반영
     - '\n'로 구분해서 comp_sum 스크립트에 전달해주므로 comp_sum에서
       또 변환할 필요 없다.

--- ledger/comp_sum.sh
+++ ledger/comp_sum.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-payments=$(cat - | gsed 's/:/\n/g' | gsed -e '/^$/d') # 빈 줄이 생길 수 있다
+payments=$(cat - | gsed -e '/^$/d') # 빈 줄이 생길 수 있다
 
 #echo "$payments"
 (echo "$payments" | cut -f3 | tr '\012' '+'; echo "0") | bc
