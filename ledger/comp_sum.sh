0ae5de8046eef1ebd0d5cda714ace4fedd7059ac
sehun-kim <rogarithm@gmail.com>
2023-03-23 01:20:21 +0900

    코드 정리
     - \012보다는 \n이 한 눈에 알아보기 쉽다

--- ledger/comp_sum.sh
+++ ledger/comp_sum.sh
@@ -2,4 +2,4 @@
 payments=$(cat - | gsed -e '/^$/d') # 빈 줄이 생길 수 있다
 
 #echo "$payments"
-(echo "$payments" | cut -f3 | tr '\012' '+'; echo "0") | bc
+(echo "$payments" | cut -f3 | tr '\n' '+'; echo "0") | bc
