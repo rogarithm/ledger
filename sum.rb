25122a47ae2871472c02143a09ff79b07042373f
sehun-kim <rogarithm@gmail.com>
2024-04-16 01:25:02 +0900

    변수 이름을 다듬는다

--- sum.rb
+++ sum.rb
@@ -1,30 +1,31 @@
 #!/usr/bin/env ruby
 
 # 지출 내역을 읽어온다
-pay_list = File.read('../ledger/2024_3')
+raw_monthly_expenses = File.read('../ledger/2024_3')
 
-all_info = {}
-current_month_date = ''
-pay_list.split().each do |elem|
-  if elem =~ /^\d+\/\d+$/ # 월/일 형식을 키로 넣는다
-#    puts "putting #{elem} as key..."
-    all_info[elem] = []
-    current_month_date = elem
-  elsif elem =~ /^$/
+monthly_expenses = {}
+current_date = ''
+raw_monthly_expenses.split().each do |line|
+  if line =~ /^\d+\/\d+$/ # 날짜는 키로 쓴다
+#    puts "putting #{line} as key..."
+    current_date = line
+    monthly_expenses[current_date] = []
+  elsif line =~ /^$/
     next
-  else # 다음 월/일 형식이 나오기 전까지 지출 내역을 추가한다
-#    puts "putting #{elem} as value..."
-    all_info[current_month_date] << elem
+  else # 다음 날짜 전까지 지출 내역을 추가한다
+#    puts "putting #{line} as value..."
+    expense = line
+    monthly_expenses[current_date] << expense
   end
 end
 
-#p all_info
+#p monthly_expenses
 
-sum_all = 0
-all_info.each do |info_by_day|
+total_expense = 0
+monthly_expenses.each do |info_by_day|
   pays_by_day = info_by_day[1].map {|p| p.to_i}
   sum_by_day = pays_by_day.sum
-  sum_all += sum_by_day
+  total_expense += sum_by_day
   puts "sum of #{info_by_day[0]} is: #{sum_by_day} ... #{info_by_day[1]}"
 end
-puts sum_all
+puts total_expense
