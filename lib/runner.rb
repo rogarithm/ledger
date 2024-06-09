dbcbf5ecc847750e56c176731d8a475bb94c45d9
sehun-kim <rogarithm@gmail.com>
2024-06-09 16:54:01 +0900

    코드 정리한다
     - 디렉토리 옮긴다
     - 테스트할 수 있게 구조를 정리한다

---
+++ lib/runner.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+require_relative "./expense"
+require_relative "./sum"
+
+# 지출 내역을 읽어온다
+if ARGV.length > 1
+  puts "more input than expected!"
+  puts "exit..."
+  return
+end
+
+raw_monthly_expenses = File.read("../ledger/#{ARGV[0]}")
+
+
+monthly_expenses = create_expense_list(raw_monthly_expenses)
+
+total_expense = 0
+monthly_expenses.each do |info_by_day|
+  pays_by_day = info_by_day[1]
+  sum_by_day = pays_by_day.map(&:amount).sum
+  total_expense += sum_by_day
+  puts "#{info_by_day[0]}... #{sum_by_day}
+  #{info_by_day[1].map(&:to_s).join("\n  ")}"
+end
+puts total_expense
+
---
+++ lib/sum.rb
@@ -0,0 +1,22 @@
+require_relative "./expense"
+
+class Sum
+  def self.create_expense_list(raw_monthly_expenses)
+    monthly_expenses = {}
+    current_date = ''
+
+    raw_monthly_expenses.split("\n").each do |line|
+      if line =~ /^\s*\d+\/\d+$/
+        current_date = line.strip
+        monthly_expenses[current_date] = []
+      elsif line =~ /^$/
+        next
+      else
+        expense = Expense.new(line)
+        monthly_expenses[current_date] << expense
+      end
+    end
+
+    monthly_expenses
+  end
+end
