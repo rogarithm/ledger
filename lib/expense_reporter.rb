a90a81ee77c2f51ea594e04a2d55051f9662299e
sehun-kim <rogarithm@gmail.com>
2024-06-10 00:30:13 +0900

    코드 정리한다
     - 지출합 계산 코드를 메서드로 분리한다

---
+++ lib/expense_reporter.rb
@@ -0,0 +1,17 @@
+require_relative './expense'
+
+class ExpenseReporter
+  def self.compute_total_expense monthly_expenses
+    total_expense = 0
+
+    monthly_expenses.each do |info_by_day|
+      pays_by_day = info_by_day[1]
+      sum_by_day = pays_by_day.map(&:amount).sum
+      total_expense += sum_by_day
+      puts "#{info_by_day[0]}... #{sum_by_day}
+  #{info_by_day[1].map(&:to_s).join("\n  ")}"
+    end
+
+    puts total_expense
+  end
+end
--- lib/runner.rb
+++ lib/runner.rb
@@ -2,6 +2,7 @@
 
 require_relative "./expense"
 require_relative "./expense_reader"
+require_relative "./expense_reporter"
 
 # 지출 내역을 읽어온다
 if ARGV.length > 1
@@ -15,13 +16,5 @@ raw_monthly_expenses = File.read("../ledger/#{ARGV[0]}")
 
 monthly_expenses = ExpenseReader.create_expense_list(raw_monthly_expenses)
 
-total_expense = 0
-monthly_expenses.each do |info_by_day|
-  pays_by_day = info_by_day[1]
-  sum_by_day = pays_by_day.map(&:amount).sum
-  total_expense += sum_by_day
-  puts "#{info_by_day[0]}... #{sum_by_day}
-  #{info_by_day[1].map(&:to_s).join("\n  ")}"
-end
-puts total_expense
 
+ExpenseReporter.compute_total_expense monthly_expenses
