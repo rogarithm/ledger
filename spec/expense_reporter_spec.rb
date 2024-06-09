d267612b415456f5ed8b9d0fbea02e34afef6ac4
sehun-kim <rogarithm@gmail.com>
2024-06-10 00:45:54 +0900

    테스트 추가한다
     - 지출내역 출력 코드를 메서드로 분리한다

---
+++ spec/expense_reporter_spec.rb
@@ -0,0 +1,16 @@
+require_relative "../lib/expense"
+require_relative "../lib/expense_reporter"
+
+require_relative "../spec/helper/spec_helper"
+
+RSpec.describe ExpenseReporter, "expense reporter" do
+  it "computes sum of all expense from expense list" do
+    expense_list = {"4/2" => [
+      Expense.new("4100,아침,맥모닝"),
+      Expense.new("1000,여가,네이버 시리즈"),
+      Expense.new("1000,커피")
+    ]}
+
+    expect(ExpenseReporter.compute_total_expense expense_list).to eq(6100)
+  end
+end
