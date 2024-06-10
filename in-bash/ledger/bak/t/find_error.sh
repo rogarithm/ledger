35669a7157fec9904b7f521d9fffc56c8101703e
sehun-kim <rogarithm@gmail.com>
2024-06-10 23:28:05 +0900

    bash로 짠 코드를 지운다

--- in-bash/ledger/bak/t/find_error.sh
+++
@@ -1,2 +0,0 @@
-echo $1
-echo $(../lefts.sh ${1}) '+' $(../range_by_account.sh 01.01 $2 ${1}) | bc
--- in-bash/notify.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-LEDGER="/Users/sehun/personal/ledger/$(date -v -1d "+%Y-%m").txt"
-RESULT=$(grep -e "^$(date -v -1d "+%m.%d")" ${LEDGER})
-YESTERDAY=$(date -v -1d "+%m.%d")
-
-if [ -z ${RESULT} ] ; then
-	echo display notification \"어제 \(${YESTERDAY}\) 지출 내역을 작성하지 않으셨네요. \" with title \"가계부\"  | osascript
-fi
