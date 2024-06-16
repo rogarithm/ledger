c5bebd23db1da8f954ee07b78f800ad1eab06e3e
sehun-kim <rogarithm@gmail.com>
2024-06-16 16:51:20 +0900

    실행 옵션 정리한다
     - --range 옵션 처리 로직을 추가한다
     - --list는 아무 옵션 주어지지 않았을 때 실행한다

--- lib/runner.rb
+++ lib/runner.rb
@@ -18,10 +18,10 @@ class Runner
     case @option.first
     when "--sum", "-s"
       puts @fm_reporter.compute_total_expense expenses
-    when "--list", "-l"
-      puts @fm_reporter.print_report expenses
     when "--range", "-r"
-      puts @fm_reporter.list_in_range expenses
+      from = @option[1]
+      to = @option[2]
+      puts @fm_reporter.list_in_range from, to, expenses
     when "--filter", "-f"
       #TODO implement filter function
     else
@@ -30,7 +30,9 @@ class Runner
   end
 
   def read_expense
-    if @option.first.start_with? "-"
+    if @option.first == "--range" or @option.first == "-r"
+      File.read("../ledger/#{@option[3]}")
+    elsif @option.first.start_with? "-"
       File.read("../ledger/#{@option[1]}")
     else
       File.read("../ledger/#{@option.first}")
