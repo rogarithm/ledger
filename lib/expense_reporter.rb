cf9ad2d37221ed6c93e83ce67bf689808d51d72f
sehun-kim <rogarithm@gmail.com>
2024-07-26 00:07:55 +0900

    카테고리별 지출내역을 포맷팅할 수 있다
     - 개별 카테고리의 지출내역은 포맷팅되지만, 서로 다른 카테고리까지
       고려하지 못하고 있다

--- lib/expense_reporter.rb
+++ lib/expense_reporter.rb
@@ -11,9 +11,13 @@ class ExpenseReporter
   end
 
   def report_by_cat expenses, category
+    max_expense = expenses.max {|e1, e2| e1.amount.to_s.size <=> e2.amount.to_s.size}
+    max_size = max_expense.amount.to_s.size
+
     result = [category]
     expenses.each do |expense|
-      result << "#{expense.at.strftime "%m/%d"}...#{expense.format_amount}"
+      dots_to_pad = '.' * (max_size - expense.amount.to_s.size)
+      result << "#{expense.at.strftime "%m/%d"}...#{dots_to_pad}#{expense.format_amount}"
     end
     result.join("\n")
   end
